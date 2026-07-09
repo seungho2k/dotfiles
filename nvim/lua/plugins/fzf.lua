ba01192989234065c1777e564a2e8bf3d758dc2d
Seungho Lee <kayalee2k@gmail.com>
2026-07-09 20:11:21 +0900

    fix conflicting keymap

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -9,7 +9,7 @@ return {
     config = function(_, opts)
         require("fzf-lua").setup(opts)
         vim.keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>")
-        vim.keymap.set("n", "<leader>g", "<cmd>FzfLua live_grep<CR>")
+        vim.keymap.set("n", "<leader>G", "<cmd>FzfLua live_grep<CR>")
         vim.keymap.set("n", "<leader>R", "<cmd>FzfLua lsp_references<CR>")
         vim.keymap.set("n", "<leader>d", "<cmd>FzfLua lsp_definitions<CR>")
         vim.keymap.set("n", "<leader>D", "<cmd>FzfLua lsp_declarations<CR>")
